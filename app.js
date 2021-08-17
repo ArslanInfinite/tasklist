cf8fbc4535db2aa19a83ef78ffc0c29208becdf7
Arslan <ali.arslan319@gmail.com>
2021-08-17 18:22:45 -0400

    initiate removeTask function that will remove a task from the UI when the a tag is clicked

--- app.js
+++ app.js
@@ -7,29 +7,26 @@ const taskInput = document.querySelector('#task')
 
 // Loading event listeners 
 loadeventlisteners()
-// Create a function that adds an eventlistener to the form 
-/// which activates on submit and triggers the addTask function
+
+// Create a function that adds an eventlistener to the form which activates on submit and triggers the addTask function
 function loadeventlisteners(){
     form.addEventListener('submit', addTask)
+    taskList.addEventListener('click', removeTask)
 }
-// addTask function checks if the value the input is empty or not
-// if it is, an alert will pop up
+// addTask function checks if the value the input is empty or not if it is, an alert will pop up
 function addTask(event){
     if(taskInput.value === ''){
         alert('Add A Task!')
     }
 // creating the list element (li) if the input is not empty
     const li = document.createElement('li')
-// assigning a class name to li and assigning it as is because materialize
-// makes 'collection-items' look better
+// assigning a class name to li and assigning it as is because materialize makes 'collection-items' look better
     li.className = 'collection-item'
-// creating a new textNode and appending it to the li so that the user input 
-// is visible on the screen after it is submitted
+// creating a new textNode and appending it to the li so that the user input is visible on the screen after it is submitted
     li.appendChild(document.createTextNode(taskInput.value))
 // need the link element so that the red X icon is visible 
     const link = document.createElement('a')
-// pressing the link removes the input from the list, secondary-content makes
-// the red X appear on the right of the list element
+// pressing the link removes the input from the list, secondary-content makes the red X appear on the right of the list element
     link.className = 'delete-item secondary-content'
     link.innerHTML = '<i class="fa fa-remove"></i>'
 // appending link to the li 
@@ -39,4 +36,11 @@ function addTask(event){
 // clear input by making the default to a string
     taskInput.value = ''
     event.preventDefault()
+}
+
+// remove task
+function removeTask(event){
+    if(event.target.parentElement.classList.contains('delete-item')) {
+        event.target.parentElement.parentElement.remove()
+    }
 }
